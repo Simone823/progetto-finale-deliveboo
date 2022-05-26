185ebe66b667e8228e920f76d5a715e4d508efd3
Simone823 <simone.daglio823@outlook.it>
2022-05-26 11:03:57 +0200

    add slug fillable user model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -16,8 +17,43 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'surname', 
+        'email', 
+        'password',
+        'p_iva',
+        'business_name',
+        'business_slug',
+        'business_city',
+        'business_cap',
+        'business_address',
+        'business_image'
     ];
+
+    // public static function get unique slug
+    public static function getUniqueSlug($business_name) {
+
+        // Slug
+        $slug = Str::slug($business_name);
+
+        // Slug base
+        $slug_base = $slug;
+
+        // Counter slug
+        $counter = 1;
+
+        // Slug present
+        $slug_present = User::where('business_slug', $slug)->first();
+
+        while($slug_present) {
+            $slug = $slug_base . '-' . $counter;
+            $counter++;
+            $slug_present = User::where('business_slug', $slug)->first();
+        }
+
+        // Return slug
+        return $slug;
+    }
 
     /**
      * The attributes that should be hidden for arrays.

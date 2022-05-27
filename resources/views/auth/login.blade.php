5cee2d385bc601bdc98199d011141cf8f7678492
Simone823 <simone.daglio823@outlook.it>
2022-05-27 20:30:45 +0200

    padding container view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('metaTitle', 'DELIVEBOO DB | LOGIN')
 
 @section('content')
-<div class="container">
+<div class="container py-5 px-3">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card fw-bolder">

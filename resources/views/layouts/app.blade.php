ca1751bd3d2c1f9e89c182cca9cae55cac5dc5ac
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-01 17:46:54 +0200

    delete old header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,23 +57,76 @@
                                 </a>
                             </li>
                             
-                        @if (Route::has('register'))
+                            @if (Route::has('register'))
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">
-                                <button class="btn-standard btn-green_1 fw-bold">
-                                    Diventa nostro partner
-                                </button>
-                            </a>
-                        </li>
-
-                        @endif
-
-                        @endguest
-                        
-    
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">
+                                    <button class="btn-standard btn-green_1 fw-bold">
+                                        Diventa nostro partner
+                                    </button>
+                                </a>
+                            </li>
+                            
+                            @endif
+                            
+                            @else
+                            
+                                {{-- Link wrapper --}}
+                                <div class="link_wrapper">
+                                    
+                                    {{-- List link --}}
+                                    <ul class="lists_link list-unstyled d-flex align-items-center gap-4">
+                                        <li>
+                                            <a class="@if(Route::is('admin.homepage'))active @endif fw-bolder text-uppercase text-decoration-none" href="{{route('admin.homepage')}}">
+                                                <span class="@if(Route::is('admin.homepage')) active @endif navbar_dashboard">
+                                                    Dashboard
+                                                </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="fw-bolder text-uppercase text-decoration-none" href="{{route('admin.plates.index')}}">
+                                                <span class="@if(Route::is('admin.plates.index'))active @endif navbar_dashboard">
+                                                    Tutti i Piatti
+                                                </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="fw-bolder text-uppercase text-decoration-none" href="{{route('admin.plates.create')}}">
+                                                <span class="@if(Route::is('admin.plates.create'))active @endif navbar_dashboard">
+                                                    Crea nuovo piatto
+                                                </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                
+                                {{-- Dropdown logout --}}
+                                <div class="dropdown">
+                                    <button class="btn rounded-pill dropdown-toggle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" >
+                                        <span id="navbar_user_color">
+                                            {{ Auth::user()->name }}
+                                        </span> 
+                                    </button>
+                            
+                                    {{-- Drop item --}}
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li>
+                                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                            @endguest    
                     </ul>
                 </div>
+
+                
     
                 {{-- MENU BURGER --}}
                 <div class="collapse navbar-collapse" id="navbarNav">
@@ -92,107 +145,12 @@
             </div>
         </nav>
     </header>
-
-
-
-
-
-    boooo
-
-
-
-
-
-
-
+    
+    
+    
+    
 
     <div id="app">
-        <nav id="navbar_app" class="navbar navbar-expand-md navbar-light shadow-lg d-flex {{-- align-center fixed-top --}}">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                   <img id="header_logo_deliveboo" src="{{ asset('img/logo_white.svg') }}" alt="DeliveBoo logo">
-                </a> 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto d-flex align-items-center gap-4">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a  class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-
-                            {{-- Link wrapper --}}
-                            <div class="link_wrapper">
-
-                                {{-- List link --}}
-                                <ul class="lists_link list-unstyled d-flex align-items-center gap-4">
-                                    <li>
-                                        <a class="@if(Route::is('admin.homepage'))active @endif fw-bolder text-uppercase text-decoration-none" href="{{route('admin.homepage')}}">
-                                            <span class="@if(Route::is('admin.homepage')) active @endif navbar_dashboard">
-                                                Dashboard
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="fw-bolder text-uppercase text-decoration-none" href="{{route('admin.plates.index')}}">
-                                            <span class="@if(Route::is('admin.plates.index'))active @endif navbar_dashboard">
-                                                Tutti i Piatti
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="fw-bolder text-uppercase text-decoration-none" href="{{route('admin.plates.create')}}">
-                                            <span class="@if(Route::is('admin.plates.create'))active @endif navbar_dashboard">
-                                                Crea nuovo piatto
-                                            </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            {{-- Dropdown logout --}}
-                            <div class="dropdown">
-                                <button class="btn rounded-pill dropdown-toggle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" >
-                                    <span id="navbar_user_color">
-                                        {{ Auth::user()->name }}
-                                    </span> 
-                                </button>
-
-                                {{-- Drop item --}}
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                </ul>
-                            </div>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
         <main class="">
             @yield('content')
         </main>

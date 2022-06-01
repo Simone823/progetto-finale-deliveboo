475735cc785526ed08c50fa8f0ff2789cd25ba70
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-01 12:04:54 +0200

    insert svg home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -22,20 +22,16 @@
 
 
 
-          <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
+
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">
+                    
+                    Registrati o accedi
+                </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
+
             </ul>
           </div>
         </div>

59e40c6cba6fc9e8b0224cbce282cf867b836ae8
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-02 17:34:17 +0200

    login complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,84 +32,97 @@
 
         {{-- SECTION PRINCIPALE --}}
         <section class="login-content d-flex flex-column flex-md-row">
-
-            <div class="col-12 col-md-6 d-flex justify-content-center">
+            
+            <div class="col-12 col-md-6 p-3 d-flex justify-content-center align-items-center">
                 <div class="form-wrapper">
-
+                    
                     <h1 class="fw-bold">Ti diamo il benvenuto su Deliveboo</h1>
                     <p>Sfrutta i dati a disposizione per far crescere il tuo business. Monitora le vendite, controlla i tuoi progressi e attira nuovi clienti con offerte speciali.</p>
-
                     
-                    {{-- Email --}}
-                    <div class="form-group mb-3 flex-column">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail*') }}</label>
-
-                        <div class="">
-                            <input placeholder="Indirizzo E-mail" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
-
-                            @error('email')
+                    
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+            
+                        
+                        {{-- Email --}}
+                        <div class="form-group mb-2 flex-column">
+                            <label for="email" class="fw-bold col-md-4 col-form-label text-md-right">
+                                {{ __('Email') }}
+                            </label>
+                            
+                            <div class="">
+                                <input placeholder="Indirizzo E-mail" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                                
+                                @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    {{-- Passowrd --}}
-                    <div class="form-group mb-3 flex-column">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password*') }}</label>
-
-                        <div class="">
-                            <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
-
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    {{-- Remember check --}}
-                    <div class="form-group mb-3 flex-column align-items-center">
-                        <div class="col-md-7">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
+                                @enderror
                             </div>
                         </div>
-                    </div>
+                        
+                        {{-- Passowrd --}}
+                        <div class="form-group mb-2 flex-column">
+                            <label for="password" class="fw-bold col-md-4 col-form-label text-md-right">
+                                {{ __('Password') }}
+                            </label>
+                            
+                            <div class="">
+                                <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
+                                
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        {{-- Remember check --}}
+                        <div class="form-group mb-3 flex-column align-items-center">
+                            <div class="col-md-7">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        {{-- Login btn --}}
+                        <div class="form-group mb-3">
+                            <button type="submit" class="fw-bold btn-standard login-button btn-green_1">
+                                {{ __('Log in') }}
+                            </button>                            
+                        </div>
 
-                    {{-- Login btn --}}
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-green_1 btn-hover-violet fw-bolder letter-spacing-2">
-                            {{ __('Login') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link fw-bolder" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
+                        {{-- PASSWORD DIMENTICATA --}}
+                        <div class="forgot-psw">
+                            @if (Route::has('password.request'))
+                            <a class="text-reset text-decoration-none" href="{{ route('password.request') }}">
+                                {{ __('Password dimenticata?') }}
                             </a>
-                        @endif
+                            @endif
+                        </div>
+                    </form>
+                </div>
+            </div>
+                
+                
+                <div class="col-12 col-md-6 login-img">
+                    <div class="img-wrapper">
+                        <img src="{{ asset('img/del_login.svg') }}" alt="">
                     </div>
+                    
+                    <div class="login-background">
+                        
+                    </div>                
                 </div>
-            </div>
-
-
-            <div class="col-12 col-md-6 login-img">
-                <div class="img-wrapper">
-                    <img src="{{ asset('img/del_login.svg') }}" alt="">
-                </div>
-
-                <div class="login-background">
-
-                </div>                
-            </div>
-
+                
         </section>
+
     </main>
 </body>
 </html>

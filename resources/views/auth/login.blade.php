eb15d86cd47efda5a98ff289fbb718c69758e4e1
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-02 16:14:46 +0200

    reset class form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,18 +33,18 @@
         {{-- SECTION PRINCIPALE --}}
         <section class="login-content d-flex flex-column flex-md-row">
 
-            <div class="col-12 col-md-6 py-5 px-3">
-                <div class="row justify-content-center">
+            <div class="col-12 col-md-6 d-flex justify-content-center">
+                <div class="form-wrapper">
 
-                    <h1>Ti diamo il benvenuto su Deliveboo</h1>
+                    <h1 class="fw-bold">Ti diamo il benvenuto su Deliveboo</h1>
                     <p>Sfrutta i dati a disposizione per far crescere il tuo business. Monitora le vendite, controlla i tuoi progressi e attira nuovi clienti con offerte speciali.</p>
 
                     
                     {{-- Email --}}
-                    <div class="form-group row mb-3 flex-column align-items-center text-center">
+                    <div class="form-group mb-3 flex-column">
                         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail*') }}</label>
 
-                        <div class="col-md-6">
+                        <div class="">
                             <input placeholder="Indirizzo E-mail" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
 
                             @error('email')
@@ -56,10 +56,10 @@
                     </div>
 
                     {{-- Passowrd --}}
-                    <div class="form-group row mb-3 flex-column align-items-center text-center">
+                    <div class="form-group mb-3 flex-column">
                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password*') }}</label>
 
-                        <div class="col-md-6">
+                        <div class="">
                             <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
 
                             @error('password')
@@ -71,9 +71,9 @@
                     </div>
 
                     {{-- Remember check --}}
-                    <div class="form-group row mb-3 flex-column align-items-center">
+                    <div class="form-group mb-3 flex-column align-items-center">
                         <div class="col-md-7">
-                            <div class="form-check d-flex justify-content-center gap-2">
+                            <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                 <label class="form-check-label" for="remember">
@@ -84,24 +84,17 @@
                     </div>
 
                     {{-- Login btn --}}
-                    <div class="form-group mb-0">
-                        <div class="col-md-8 m-auto text-center">
-                            <button type="submit" class="btn btn-green_1 btn-hover-violet fw-bolder letter-spacing-2">
-                                {{ __('Login') }}
-                            </button>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-green_1 btn-hover-violet fw-bolder letter-spacing-2">
+                            {{ __('Login') }}
+                        </button>
 
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link fw-bolder" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link fw-bolder" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
                     </div>
-
-
-
-
-
                 </div>
             </div>
 

eb0f1c79f1d6ac077633140caba8e6eda3535a24
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-03 13:09:22 +0200

    change checkbox

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,46 +4,136 @@
 
 @section('content')
 <div class="container-md">
-    <div class="register-wrapper col-6">
-
-        <h1 class="fw-bold">Diventa un partner di Deliveboo</h1>
-
+    <div class="register-wrapper col-10 col-sm-8 col-md-6 m-auto m-md-0">
+        
+        <h1 class="fw-bold mb-4">Diventa un partner di Deliveboo</h1>
+        
         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
             @csrf
-        
-            {{-- Name --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Inserisci il tuo Nome" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
-        
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Surname --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Inserisci il tuo Cognome" id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
-        
-                    @error('surname')
+            
+            {{-- Business name --}}
+            <div class="form-group row mb-2 flex-column text-center">
+                
+                <div class="">
+                    <input placeholder="Nome del ristorante" id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" autocomplete="business_name" autofocus>
+                    
+                    @error('business_name')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
                 </div>
             </div>
+            
+            {{-- Business address --}}
+            <div class="form-group row mb-2 flex-column text-center">   
+                <div class="">
+                    <input placeholder="Indirizzo e numero civico del ristorante" id="business_address" type="text" class="form-control @error('business_address') is-invalid @enderror" name="business_address" value="{{ old('business_address') }}" autocomplete="business_address" autofocus>
+                
+                    @error('business_address')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror            
+                </div>         
+            </div>
+
+            {{-- CITY - CAP --}}
+            <div class="row mb-2">
+                {{-- Business city --}}
+                <div class="form-group col-6">
+                    <input placeholder="Città o paese" id="business_city" type="text" class="form-control @error('business_city') is-invalid @enderror" name="business_city" value="{{ old('business_city') }}" autocomplete="business_city" autofocus>
+        
+                    @error('business_city')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            
+                {{-- Business cap --}}
+                <div class="form-group col-6">
+                    <input placeholder="CAP" id="business_cap" type="number" class="form-control @error('business_cap') is-invalid @enderror" name="business_cap" value="{{ old('business_cap') }}" autocomplete="business_cap" autofocus>
+                    
+                    @error('business_cap')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+            
+            {{-- Partita iva --}}
+            <div class="form-group row mb-2 flex-column text-center">
+        
+                <div class="">
+                    <input placeholder="Partita Iva" id="p_iva" type="number" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" autocomplete="p_iva" autofocus>
+        
+                    @error('p_iva')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+              
+
+            {{-- Tipologia ristorante --}}
+            <div class="types row pt-2 mb-2">
+                {{-- Info--}}
+                <h6 class="fw-bolder">Tipo di cucina (min 1)</h6>
+                {{-- Form checkbox --}}
+
+                <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                    @foreach($types as $index => $type)
+                        <input type="checkbox" class="btn-check" id="types-{{$type->id}}" value="{{$type->id}}" name="types[{{$index}}]"autocomplete="off">
+
+                        <label class="btn btn-outline-primary" for="types-{{$type->id}}">{{$type->type_name}}</label>
+                    @endforeach
+                </div>
+
+                {{-- Errors types --}}
+                @error('types')
+                    <span class="alert alert-danger">{{ $message . ' min: 1' }}</span>
+                @enderror
+            </div>
+
+
+            {{-- NOME - COGNOME --}}
+            <div class="row mb-2">                
+                {{-- Name --}}
+                <div class="form-group col-6">
+            
+                    <input placeholder="Nome" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+        
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                 
+                </div>
+            
+                {{-- Surname --}}
+                <div class="form-group col-6">
+            
+                    <input placeholder="Cognome" id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
+        
+                    @error('surname')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                
+                </div>
+            </div>
+        
         
             {{-- Email --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
+            <div class="form-group row mb-2 flex-column text-center">
         
-                <div class="col-md-6">
-                    <input placeholder="Indirizzo Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                <div class="">
+                    <input placeholder="Indirizzo email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
         
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -54,9 +144,9 @@
             </div>
         
             {{-- Passowrd --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
+            <div class="form-group row mb-2 flex-column text-center">
         
-                <div class="col-md-6">
+                <div class="">
                     <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
         
                     @error('password')
@@ -68,101 +158,13 @@
             </div>
         
             {{-- Password confirm --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
+            <div class="form-group row mb-2 flex-column text-center">
         
-                <div class="col-md-6">
+                <div class="">
                     <input placeholder="Conferma Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                 </div>
             </div>
         
-            {{-- Business name --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Nome Ristorante" id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" autocomplete="business_name" autofocus>
-        
-                    @error('business_name')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Partita iva --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Partita Iva" id="p_iva" type="number" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" autocomplete="p_iva" autofocus>
-        
-                    @error('p_iva')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Business city --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Città" id="business_city" type="text" class="form-control @error('business_city') is-invalid @enderror" name="business_city" value="{{ old('business_city') }}" autocomplete="business_city" autofocus>
-        
-                    @error('business_city')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Business cap --}}
-            <div class="form-group row mb-3 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="CAP" id="business_cap" type="number" class="form-control @error('business_cap') is-invalid @enderror" name="business_cap" value="{{ old('business_cap') }}" autocomplete="business_cap" autofocus>
-        
-                    @error('business_cap')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Business address --}}
-            <div class="form-group row mb-4 flex-column align-items-center text-center">
-        
-                <div class="col-md-6">
-                    <input placeholder="Indirizzo Ristorante" id="business_address" type="text" class="form-control @error('business_address') is-invalid @enderror" name="business_address" value="{{ old('business_address') }}" autocomplete="business_address" autofocus>
-        
-                    @error('business_address')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-        
-            {{-- Tipologia ristorante --}}
-            <div class="types row mb-3">
-                {{-- Info--}}
-                <h5 class="fw-bolder text-center mb-3">Tipologie Ristorante* (Minimo 1)</h5>
-                {{-- Form checkbox --}}
-                <ul class="form-group d-flex flex-wrap gap-2">
-                    @foreach($types as $index => $type)
-                        <li class="form-group form-check">
-                            <input type="checkbox" class="form-check-input @error('types.{{$index}}') is-invalid @enderror" value="{{$type->id}}" name="types[{{$index}}]" id="types-{{$type->id}}">
-                            <label class="form-check-label" for="types-{{$type->id}}">{{$type->type_name}}</label>
-                        </li>
-                    @endforeach
-                </ul>
-                {{-- Errors types --}}
-                @error('types')
-                    <span class="alert alert-danger">{{ $message . ' min: 1' }}</span>
-                @enderror
-            </div>
         
             {{-- Business image input--}}
             <div class="form-group row mb-3 flex-column align-items-center text-center">

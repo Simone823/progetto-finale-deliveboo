a5af229a0ee6a92dd81b83ba8f7bf7179a448d1f
alemica97 <alessandro.micalizzi08@gmail.com>
2022-06-03 17:59:50 +0200

    modifica cards menu

--- resources/js/pages/ResturantMenu.vue
+++ resources/js/pages/ResturantMenu.vue
@@ -27,28 +27,21 @@
                 <div class="container pt-5">
                     <h3>Ecco il nostro menù</h3>
                     <!-- ciclo il componente MenuCard per stampare tutti i piatti  -->
-                    <div class="cards-wrapper row justify-content-center">
-                        <div class="card-menu col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+                    <div class="cards-wrapper row justify-content-start">
+                        <div class="card-menu col-12 col-md-6 col-lg-4 gap-2 mb-3"
                             v-for="(menuPlate,index) in menuPlates" :key="index"
                             v-on:click="viewPlate(index)">
-                            <div class="card-wrapper">
-                                <div class="card-header p-0">
+                            <div class="card-wrapper p-3 d-flex justify-content-between">
+                                <div class="card-title flex-grow-1">
+                                    <h5>{{ menuPlate.name }}</h5>
+                                </div>
+                                <div class="align-self-center justify-content-center">
                                     <figure class="post-card-img m-0">
                                         <!-- TODO inserire immagine -->
                                         <img src="https://i.picsum.photos/id/431/5398/3599.jpg?hmac=bc325kFqsm626RGhgs-XwG_GFqd4x3VmXtramO12qL8" alt="">
                                     </figure>
                                 </div>
-                                <div class="card-body">
-                                    <h5>{{ menuPlate.name }}</h5>
-                                    <div class="info-wrapper">
-                                        <span class="ingredients m-0">Ingredienti:</span>
-                                        <p class="ingredients-list m-0">{{ menuPlate.ingredients }}</p>
-                                        <span class="price">Prezzo: {{ menuPlate.price }}&euro;</span>
-                                    </div>
-                                </div>
-                                <div class="card-footer">
-                                    qui ci andrà il pulsante (- / +)
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -57,7 +50,7 @@
             <!-- gestione del componente attivo -->
             <div class="info-wrapper">
                 <div :class=" [activeElement != undefined ? 'active' : '','info-plate-card'] ">
-                    <h1></h1>
+                    <h1>cavallo</h1>
                 </div>
             </div>
         </main>
@@ -66,13 +59,8 @@
 </template>
 
 <script>
-import MenuCard from '../components/MenuCard.vue'
 
 export default {
-
-    components: {
-        MenuCard,
-    },
 
     data() {
         return {
@@ -96,7 +84,7 @@ export default {
         },
         viewPlate(i){
             this.activeElement = i;
-            console.log(this)
+            console.log(this.activeElement)
         }
     },
     mounted() {
@@ -117,12 +105,22 @@ export default {
         }
     }
 
-    main{
-        position: relative;
-    }
-    .post-card-img{
-            width: 100%;
-            aspect-ratio: 5/4;
+    .card-wrapper{
+        cursor: pointer;
+        border-radius: 5px;
+        overflow: hidden;
+        background-color: beige;
+        height: 132px;
+
+        &:hover img{
+            transform: scale(1.1,1.1);
+            transition: all 500ms;
+        }
+
+        .post-card-img{
+            width: 100px;
+            height: 100px;
+            flex-shrink: 0;
             overflow: hidden;
             img{
                 width: 100%;
@@ -130,63 +128,17 @@ export default {
                 object-fit: cover;
                 object-position: center;
             }
-            &:hover img{
-                transform: scale(1.1,1.1);
-                transition: all 500ms;
-            }
-        }
-
-    .card-wrapper{
-        cursor: pointer;
-        border-radius: 10px;
-        overflow: hidden;
-        width: 100%;
-        aspect-ratio: 9/16;
-        position: relative;
-        background-color: beige;
-
-        .ingredients{
-            font-weight: 600;
-        }
-
-        .ingredients-list{
-            height: 50px;
-            overflow-y: auto;
-            overflow-x: hidden;
-            padding: 0 10px 0 0;
-        }
-        
-        .card-footer{
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            right: 0;
-        }
-
-        ::-webkit-scrollbar {
-            width: 5px;
-        }
-
-        ::-webkit-scrollbar-track{
-            background-color: #D0EB99;
-            border-radius: 100px;
-            margin-right: 0;
-        }
-
-        ::-webkit-scrollbar-thumb{
-            background-color: #00CCBC;
-            border-radius: 100px;
-        }
-
-        ::-webkit-scrollbar-thumb:hover{
-            background-color: #14ebd9;
         }
     }
+
     .info-wrapper{
         width: 100%;
         height: 100vh;
         position: absolute;
+        top: 0;
+        z-index: -9999;
     }
+
     .info-plate-card{
         position: absolute;
         top: 50%;

c70a2176ca2eca1daa72b9602fff3fe160a7da8d
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-05 16:53:12 +0200

    setting new plate

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -1,75 +1,117 @@
 @extends('layouts.app')
 
-@section('metaTitle', 'DELIVEBOO DB | CREA NUOVO PIATTO')
+@section('metaTitle', 'Deliveboo - new plate')
 
 @section('content')
 
-<div class="container py-5 px-3">
+<div class="register-wrapper modify-wrapper container-md pt-4 d-flex justify-content-center">
+    <div class="col-10 col-sm-8 col-md-6 m-auto m-md-0">
+        
+        <h1 class="fw-bold mb-4">Crea un nuovo Piatto</h1>
+        
+        <form action="{{ route('admin.plates.store') }}" method="POST" enctype="multipart/form-data">
+            {{-- Key --}}
+            @csrf
 
-    {{-- Title --}}
-    <h1>Crea nuovo Piatto</h1>
+            {{-- NOME PIATTO --}}
+            <div class="form-group row mb-2 flex-column">
+                <h6 class="fw-bold">Nome del piatto</h6>
+                <div>
+                    <input placeholder="Nome del piatto"
+                    id="name" type="text" class="form-control
+                    @error('name') is-invalid @enderror"
+                    name="name" value="{{ old('name') }}"
+                    autocomplete="name">
+                    
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div> 
+            </div>
 
-    <form action="{{ route('admin.plates.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
 
-        {{-- name --}}
-        <div class="form-group mb-3">
-            <label for="name">Nome Piatto</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" placeholder="Insert plate name">
-            @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+            {{-- IGREDIENTI --}}
+            <div class="form-group row mb-2 flex-column">
+                <h6 class="fw-bold">Ingredienti</h6>
+                <div>
+                    <textarea placeholder="Inserisci gli ingredienti separandoli con una virgola"
+                    id="ingredients" type="text" class="form-control 
+                    @error('ingredients') is-invalid @enderror" rows="3"
+                    name="ingredients">
+                    {{ old('ingredients') }}
+                    </textarea>
+                    
+                    @error('ingredients')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>                
+            </div>
 
-        {{-- ingredients --}}
-        <div class="form-group mb-3">
-            <label for="ingredients">Ingredienti / Descrizione</label>
-            <textarea type="text" class="form-control @error('ingredients') is-invalid @enderror" name="ingredients" id="ingredients" rows="3" placeholder="Enter the ingredients separated by ','">
-                {{ old('ingredients') }}
-            </textarea>
-            @error('ingredients')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
 
-        {{-- price --}}
-        <div class="form-group mb-3">
-            <label for="price">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}" placeholder="Inserisci il prezzo in formato: XX.XX">
-            @error('price')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+            {{-- Prezzo --}}
+            <div class="form-group row mb-2 flex-column">
+                <h6 class="fw-bold">Prezzo</h6>
+                <div>
+                    <input placeholder="Inserisci il prezzo in formato: XX.XX"
+                    id="price" type="text" class="form-control
+                    @error('price') is-invalid @enderror"
+                    name="price" value="{{ old('price') }}"
+                    autocomplete="price">
+                    
+                    @error('price')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div> 
+            </div>
 
-        {{-- visibility --}}
-        <div class="form-group mb-3">
-            <label for="visibility">Visibilità</label>
-            <select class="form-select @error('visibility') is-invalid @enderror" aria-label="Default select example" name="visibility" id="visibility">
-                <option selected disabled>Seleziona una visibilità</option>
-                <option {{ old('visibility') == '0' ? 'selected' : '' }} value="0">Non Visibile</option>
-                <option {{ old('visibility') == '1' ? 'selected' : '' }} value="1">Visibile</option>
-            </select>
-            @error('visibility')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
 
-        {{-- image --}}
-        <div class="form-group mb-3">
-            <label for="image" class="col-md-4 col-form-label text-md-right">Immagine</label>
-            <div class="col-md-6">
-                <input accept="image/*" placeholder="Immagine Ristorante" id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" autofocus>
+            {{-- visibile --}}
+            <div class="form-group mb-2">
+                <h6 class="fw-bold">Disponibile</h6>
 
-                @error('image')
+                <select class="form-select @error('visibility') is-invalid @enderror" aria-label="Default select example" name="visibility" id="visibility">
+                    <option selected disabled>Seleziona una visibilità</option>
+                    <option {{ old('visibility') == '0' ? 'selected' : '' }} value="0">Non Visibile</option>
+                    <option {{ old('visibility') == '1' ? 'selected' : '' }} value="1">Visibile</option>
+                </select>
+
+                @error('visibility')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
-        </div>
 
-        {{-- Button --}}
-        <div class="container px-0 py-3">
-            <button type="submit" class="btn btn-green_1 btn-hover-violet px-4">Aggiungi</button>
-        </div>
 
-    </form>
+
+            {{-- plate image input--}}
+            <div class="form-group mb-4">
+                <h6 class="fw-bold">Immagine del piatto</h6>
+        
+                <div class="col-lg-8">
+                    <input accept="image/*" placeholder="Immagine del piatto" id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}">
+        
+                    @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+
+        
+            {{-- Button --}}
+            <div class="form-group mb-0 pb-5 d-flex gap-3">
+                <button type="submit" class="btn-standard btn-green_1">
+                    Aggiungi
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
 @endsection

68e279107a3b5ba81fa626f8f9afb7ad6316fd39
Simone823 <simone.daglio823@outlook.it>
2022-06-06 18:04:31 +0200

    fix img homepage

--- resources/views/admin/homepage.blade.php
+++ resources/views/admin/homepage.blade.php
@@ -130,7 +130,7 @@
                           
               <div class="carousel-item {{ $key == count($lastPlates) - 1 ? 'active' : ''}}">
 
-                <img src="{{$plate->image ? asset('storage/'.$plate->image) : asset('img/placeholder_plate.png')}}" class="d-block w-100">
+                <img src="{{asset('storage/'.$plate->image)}}" class="d-block w-100">
                 
                 <div class="carousel-caption">
                   <h5 class="caption-text">{{ $plate->name }}</h5>

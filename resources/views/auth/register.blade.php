477c3055db68f3fbf07bee462fcdcceaea5ceaf7
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-07 01:27:43 +0200

    validate client side page register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
             <div class="form-group row mb-2 flex-column">
                 
                 <div class="">
-                    <input placeholder="Nome del ristorante" id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" autocomplete="business_name" autofocus>
+                    <input required placeholder="Nome del ristorante" id="business_name" type="text" class="form-control @error('business_name') is-invalid @enderror" name="business_name" value="{{ old('business_name') }}" autocomplete="business_name" autofocus>
                     
                     @error('business_name')
                     <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
             {{-- Business address --}}
             <div class="form-group row mb-2 flex-column">   
                 <div class="">
-                    <input placeholder="Indirizzo e numero civico del ristorante" id="business_address" type="text" class="form-control @error('business_address') is-invalid @enderror" name="business_address" value="{{ old('business_address') }}" autocomplete="business_address" autofocus>
+                    <input required placeholder="Indirizzo e numero civico del ristorante" id="business_address" type="text" class="form-control @error('business_address') is-invalid @enderror" name="business_address" value="{{ old('business_address') }}" autocomplete="business_address" autofocus>
                 
                     @error('business_address')
                     <span class="invalid-feedback" role="alert">
@@ -42,7 +42,7 @@
             <div class="row mb-2">
                 {{-- Business city --}}
                 <div class="form-group col-6">
-                    <input placeholder="Città o paese" id="business_city" type="text" class="form-control @error('business_city') is-invalid @enderror" name="business_city" value="{{ old('business_city') }}" autocomplete="business_city" autofocus>
+                    <input required placeholder="Città o paese" id="business_city" type="text" class="form-control @error('business_city') is-invalid @enderror" name="business_city" value="{{ old('business_city') }}" autocomplete="business_city" autofocus>
         
                     @error('business_city')
                     <span class="invalid-feedback" role="alert">
@@ -53,7 +53,7 @@
             
                 {{-- Business cap --}}
                 <div class="form-group col-6">
-                    <input placeholder="CAP" id="business_cap" type="number" class="form-control @error('business_cap') is-invalid @enderror" name="business_cap" value="{{ old('business_cap') }}" autocomplete="business_cap" autofocus>
+                    <input required placeholder="CAP" id="business_cap" type="number" class="form-control @error('business_cap') is-invalid @enderror" name="business_cap" value="{{ old('business_cap') }}" autocomplete="business_cap" autofocus>
                     
                     @error('business_cap')
                     <span class="invalid-feedback" role="alert">
@@ -67,7 +67,7 @@
             <div class="form-group row mb-2 flex-column">
         
                 <div class="">
-                    <input placeholder="Partita Iva" id="p_iva" type="text" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" autocomplete="p_iva" autofocus>
+                    <input required placeholder="Partita Iva" id="p_iva" type="text" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" autocomplete="p_iva" autofocus>
         
                     @error('p_iva')
                     <span class="invalid-feedback" role="alert">
@@ -106,7 +106,7 @@
                 {{-- Name --}}
                 <div class="form-group col-6">
             
-                    <input placeholder="Nome" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                    <input required placeholder="Nome" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
         
                     @error('name')
                         <span class="invalid-feedback" role="alert">
@@ -119,7 +119,7 @@
                 {{-- Surname --}}
                 <div class="form-group col-6">
             
-                    <input placeholder="Cognome" id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
+                    <input required placeholder="Cognome" id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
         
                     @error('surname')
                     <span class="invalid-feedback" role="alert">
@@ -135,7 +135,7 @@
             <div class="form-group row mb-2 flex-column">
         
                 <div class="">
-                    <input placeholder="Indirizzo email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                    <input required placeholder="Indirizzo email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
         
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -149,7 +149,7 @@
             <div class="form-group row mb-2 flex-column">
         
                 <div class="">
-                    <input placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                    <input required placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
         
                     @error('password')
                         <span class="invalid-feedback" role="alert">
@@ -163,7 +163,7 @@
             <div class="form-group row mb-2 flex-column">
         
                 <div class="">
-                    <input placeholder="Conferma Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                    <input required placeholder="Conferma Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                 </div>
             </div>
         

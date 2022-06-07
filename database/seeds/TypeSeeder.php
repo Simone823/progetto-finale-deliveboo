d64d210ec9d25e512cad5514814ecf33f2a5d75b
Simone823 <simone.daglio823@outlook.it>
2022-06-07 13:13:43 +0200

    modifica path img type seeder

--- database/seeds/TypeSeeder.php
+++ database/seeds/TypeSeeder.php
@@ -17,77 +17,77 @@ class TypeSeeder extends Seeder
         $arrayTypes = [
             [
                 'name' => 'Internazionale',
-                'image' => 'uploads/cucina_internazionale-min.jpg'
+                'image' => 'uploads/cucina_internazionale.png'
             ],
 
             [
                 'name' => 'Italiano',
-                'image' => 'uploads/cucina_italiana-min.jpg'
+                'image' => 'uploads/cucina_italiana.png'
             ],
 
             [
                 'name' => 'Giapponese',
-                'image' => 'uploads/cucina_giapponese-min.jpg'
+                'image' => 'uploads/cucina_giapponese.png'
             ],
 
             [
                 'name' => 'Cinese',
-                'image' => 'uploads/cucina_cinese-min.jpg'
+                'image' => 'uploads/cucina_cinese.png'
             ],
 
             [
                 'name' => 'Indiano',
-                'image' => 'uploads/cucina_indiana-min.jpg'
+                'image' => 'uploads/cucina_indiana.png'
             ],
 
             [
                 'name' => 'Messicano',
-                'image' => 'uploads/cucina_messicana-min.jpg'
+                'image' => 'uploads/cucina_messicana.png'
             ],
 
             [
                 'name' => 'Americano',
-                'image' => 'uploads/cucina_americana-min.jpg'
+                'image' => 'uploads/cucina_americana.png'
             ],
 
             [
                 'name' => 'Pesce',
-                'image' => 'uploads/cucina_pesce-min.jpg'
+                'image' => 'uploads/cucina_pesce.png'
             ],
 
             [
                 'name' => 'Steak House',
-                'image' => 'uploads/cucina_steak_house-min.jpg'
+                'image' => 'uploads/cucina_steak_house.png'
             ],
 
             [
                 'name' => 'Greco',
-                'image' => 'uploads/cucina_greca-min.jpg'
+                'image' => 'uploads/cucina_greca.png'
             ],
 
             [
                 'name' => 'Pizzeria',
-                'image' => 'uploads/cucina_pizza-min.jpg'
+                'image' => 'uploads/cucina_pizza.png'
             ],
 
             [
                 'name' => 'Bar',
-                'image' => 'uploads/cucina_bar-min.jpg'
+                'image' => 'uploads/cucina_bar.png'
             ],
 
             [
                 'name' => 'Fast-Food',
-                'image' => 'uploads/cucina_fast_food-min.jpg'
+                'image' => 'uploads/cucina_fast_food.png'
             ],
 
             [
                 'name' => 'All you can eat',
-                'image' => 'uploads/cucina_all_you_can_eat-min.jpg'
+                'image' => 'uploads/cucina_all_you_can_eat.png'
             ],
 
             [
                 'name' => 'Spagnolo',
-                'image' => 'uploads/cucina_spagnola-min.jpg'
+                'image' => 'uploads/cucina_spagnola.png'
             ],
         ];
 

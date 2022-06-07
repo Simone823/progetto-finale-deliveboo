42de17ebb7cc1767a45cdb79e44ee887a2d65438
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-07 17:04:07 +0200

    footer complete

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,51 @@
+<div class="d-flex justify-content-center Bcolor">
+    <div class="container-md footer-container d-flex align-items-center pt-3">
+      <div class="discover-deliveboo">
+      <ul><span>Scopri Deliveboo</span>
+        <li><a href="#">Investitori</a></li>
+        <li><a href="#">Chi siamo</a></li>
+        <li><a href="#">Ristoranti</a></li>
+        <li><a href="#">Altro</a></li>
+        <li><a href="#">Pressroom</a></li>
+        <li><a href="#">Il nostro blog</a></li>
+        <li><a href="#">Programmazione</a></li>
+        <li><a href="#">Design</a></li>
+        <li><a href="#">Lavora con noi</a></li>
+        <li><a href="#">Diventa nostro partner</a></li>
+      </ul>
+      </div>
+      <div class="legal-notices">
+        <ul><span>Note legali</span>
+          <li><a href="#">Termini & Condizioni</a></li>
+          <li><a href="#">Informativa sulla privacy</a></li>
+          <li><a href="#">Cookies</a></li>
+        </ul>
+      </div>
+      <div class="help">
+        <ul><span>Aiuto</span>
+          <li><a href="#">Contatti</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Tipi di cucina</a></li>
+        </ul>
+      </div>
+      <div class="take-deliveboo">
+        <ul><span>Porta Deliveboo con te</span>
+        <li> 
+          <a href="#">
+            <img src="{{ asset('img/MyNews/android.png') }}" alt="android">
+          </a> 
+        </li>
+        <li> 
+          <a href="#">
+            <img src="{{ asset('img/MyNews/apple.png') }}" alt="apple">
+          </a> 
+        </li>
+        </ul>
+      </div>
+      <div class="name-sviluppatori text-center">
+        <span>Sito web sviluppato a puro scopo educativo da <a href="https://github.com/Simone823" target="_blank">Siomone Daglio</a>, <a href="https://github.com/ilguarnieri" target="_blank">Angelo Guarnieri</a>, <a href="https://github.com/alemica97" target="_blank">Alessandro Micalizzi</a>, <a href="https://github.com/Tredicibrando" target="_blank">Giovanni Nicolosi</a>, ispirato a Deliveroo.</span>
+    </div>
+      </div>
+    </div>
+
+</div>

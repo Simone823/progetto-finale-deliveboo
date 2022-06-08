74d87a05ad8af616723afc140e3fe4a41d8fb757
Simone823 <simone.daglio823@outlook.it>
2022-06-09 01:01:04 +0200

    payment controller

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentsController extends Controller
+{
+    public function make(Request $request) {
+        $payload = $request->input('payload', false);
+        $nonce = $payload['nonce'];
+        $status = \Braintree\Transaction::sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        return response()->json($status);
+    }
+}

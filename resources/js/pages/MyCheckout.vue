7ab58bbbb4933bb4a80f0e07baeb3eb685dd63ef
Simone823 <simone.daglio823@outlook.it>
2022-06-09 11:36:38 +0200

    my checkout pages vue

---
+++ resources/js/pages/MyCheckout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <MyHeader/>
+        <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div id="dropin-container"></div>
+                <button id="submit-button">Request payment method</button>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+import MyHeader from '../components/MyHeader.vue';
+    export default {
+        components: {
+            MyHeader,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

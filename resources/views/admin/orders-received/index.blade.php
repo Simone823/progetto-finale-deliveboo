10fc7ac9d71948c26ab1ebd1349bcf1a81fc57b7
alemica97 <alessandro.micalizzi08@gmail.com>
2022-06-10 16:22:23 +0200

    aggiunti i dati del DB al file index

--- resources/views/admin/orders-received/index.blade.php
+++ resources/views/admin/orders-received/index.blade.php
@@ -3,10 +3,12 @@
 @section('metaTitle', 'deliveboo - Piatti')
 
 @section('content')
-    <h1>qui andranno gli ordini ricevuti</h1>
-    @dd($orders)
-    {{-- @foreach ($orders as $order)
-        <p>Id dell'ordine: {{ $order->id }}</p>
-        <p>Effettuato da: {{ $order->guest_name }}{{ $order->guest_name }}</p>
-    @endforeach --}}
+    {{-- <h1>qui andranno gli ordini ricevuti</h1> --}}
+    {{-- @dd($orders) --}}
+    <div>
+        @foreach ($orders as $order)
+            <p>Id dell'ordine: {{ $order->id }}</p>
+            <p>Effettuato da: {{ $order->guest_name . ' ' . $order->guest_surname }}</p>
+        @endforeach
+    </div>
 @endsection

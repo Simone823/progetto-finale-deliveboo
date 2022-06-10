67fb2636162bbecf7673220c0fcfe035a3d433e5
Simone823 <simone.daglio823@outlook.it>
2022-06-10 14:13:28 +0200

    amount total checkout controller

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     public function checkout()
     {
+
+        // Order
+        $order = Order::orderBy('created_at', 'DESC')->first();
+
+        // Total
+        $total = $order['total_price'];
+
         // Enter Your Stripe Secret
         \Stripe\Stripe::setApiKey('sk_test_51L8o3jAgtEHU3c7wAOs4DgYaNeTCvDYJWGc7cOX1YJw8eAN4qgG9KpDHUXkG16i2P38TWHtr9Jdzzy0qolycHnbA00uMwa1yLf');
 
-        $amount = 100;
+        $amount = $total;
         $amount *= 100;
         $amount = (int) $amount;
 
         $payment_intent = \Stripe\PaymentIntent::create([
             'description' => 'Stripe Test Payment',
             'amount' => $amount,
-            'currency' => 'INR',
+            'currency' => 'EUR',
             'description' => 'Payment From Codehunger',
             'payment_method_types' => ['card'],
         ]);

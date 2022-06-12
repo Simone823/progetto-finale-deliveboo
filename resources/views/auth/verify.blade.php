8cb5f2a17ede5c8729791d7e6f6fefa8c0ab52ed
Simone823 <simone.daglio823@outlook.it>
2022-06-12 20:28:40 +0200

    modifica view auth verify blade

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,25 +1,45 @@
 @extends('layouts.app')
 
+@section('metaTitle', 'Deliveboo | Verifica Email')
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+        {{-- TURN BACK --}}
+        <div class="col-11 col-sm-8 pb-3 align-self-end">
+            <a href="{{url('/')}}" class="text-reset text-decoration-none">
+                <i class="fa-solid fa-arrow-left-long"></i>
+                <span class="text-hover-purple">Torna alla homepage</span>
+            </a>
+        </div>
+
+
+        <div class="col-11 col-sm-8">
+            <div class="card p-2 p-sm-4">
+                <h2 class="fw-bold text-center ps-2 pe-2">{{ __('Verifica il tuo indirizzo Email') }}</h2>
+
 
                 <div class="card-body">
+                    
+                    <div class="text-center fs-5">
+                        {{ __('Prima di procedere, controlla la tua email per un link di verifica.') }}
+                    </div>
+                    
+                    <div class="text-center mb-2 fs-5">
+                        {{ __('Se non avete ricevuto l\' e-mail') }}
+                    </div>
+                    
+                    <form class="" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn-standard btn-green_1 w-100">{{ __('Clicca qui per richiederne un altro') }}</button>
+                    </form>
+
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un nuovo link di verifica è stato inviato al tuo indirizzo email.') }}
                         </div>
                     @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
                 </div>
             </div>
         </div>

f7fe991cde81ca164c93ab23d99e585d05346a99
Simone823 <simone.daglio823@outlook.it>
2022-06-12 02:22:26 +0200

    validazione form ordercontroller api

--- public/js/front.js
+++ public/js/front.js
@@ -69833,7 +69833,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\alex-\ProgettiBooleanCLI\progetto-finale-deliveboo\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! D:\Boolean\Esercizi-Boolean\PROGETTO FINALE TEAM 6 (DELIVEBOO)\progetto-finale-deliveboo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
